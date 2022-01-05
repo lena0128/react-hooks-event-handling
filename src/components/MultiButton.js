9a3b42141999ff91327e897f17d2cc61626aaa73
Lena Hou <lenahou2013@gmail.com>
2022-01-05 11:54:38 -0500

    commit tree 317249aaee90c2e37d1266b5a3aa1b4a3c86a23b

--- src/components/MultiButton.js
+++ src/components/MultiButton.js
@@ -1,15 +1,18 @@
 import React from "react";
 
 function MultiButton() {
+
+
   function handleClick(number) {
     console.log(`Button ${number} was clicked`);
   }
 
+  // we always need to provide a function definition, not a function invocation to out event handler
   return (
     <div>
-      <button onClick={handleClick}>Button 1</button>
-      <button onClick={handleClick}>Button 2</button>
-      <button onClick={handleClick}>Button 3</button>
+      <button onClick={() => handleClick(1)}>Button 1</button>
+      <button onClick={() => handleClick(2)}>Button 2</button>
+      <button onClick={() => handleClick(3)}>Button 3</button>
     </div>
   );
 }
